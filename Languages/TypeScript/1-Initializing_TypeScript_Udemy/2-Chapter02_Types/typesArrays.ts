0091895342add9629a54d4f651ed414970127a8b
Thiago-RM <thiago.rmdf@gmai.com>
2022-02-16 19:21:00 -0300

    [UPDATE]: FILES OF THE CHAPTER TWO CLASS 17
    Array type lesson

--- Languages/TypeScript/1-Initializing_TypeScript_Udemy/2-Chapter02_Types/typesArrays.ts
+++ Languages/TypeScript/1-Initializing_TypeScript_Udemy/2-Chapter02_Types/typesArrays.ts
@@ -1,3 +1,7 @@
 let nomeDaAula: string = 'Aula sobre os tipos de um array';
 console.log("\n\n" + nomeDaAula);
 
+let hobbies: string[] = ["Cozinhar", "Praticar Esportes"];
+console.log(hobbies[0]);
+console.log(hobbies);
+console.log(typeof hobbies);
