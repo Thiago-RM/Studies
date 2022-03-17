db970e2628b35be996eeeb1680314a79b3045005
Thiago-RM <thiago.rmdf@gmai.com>
2022-03-16 22:20:55 -0300

    [UPDATE]: FILES OF THE CHAPTER TWO CLASS
    Class about objects and types, class 24.

---
+++ Languages/TypeScript/1-Initializing_TypeScript_Udemy/2-Chapter02_Types/TypesAndObjects.ts
@@ -0,0 +1,10 @@
+let aula24: string = 'Types and Objects';
+console.log("\n\n" + aula24);
+
+//Objetos
+let usuario: { nome: string, idade: number } = {
+    nome: "João",
+    idade: 27
+}
+
+console.log(usuario)
