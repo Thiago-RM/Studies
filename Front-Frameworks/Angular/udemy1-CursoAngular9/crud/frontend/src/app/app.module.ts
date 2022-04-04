4c21ba9307857134d41eea998fe419ff63f89a68
Thiago-RM <thiago.rmdf@gmai.com>
2022-04-03 22:28:25 -0300

    [FEAT]: Classroom directives in practice
    Class for creating the product screen, as well as the model responsible for the inserted products.

--- Front-Frameworks/Angular/udemy1-CursoAngular9/crud/frontend/src/app/app.module.ts
+++ Front-Frameworks/Angular/udemy1-CursoAngular9/crud/frontend/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { RedDirective } from './directves/red.directive';
 import { ForDirective } from './directves/for.directive';
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -38,7 +39,8 @@ import { ForDirective } from './directves/for.directive';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
