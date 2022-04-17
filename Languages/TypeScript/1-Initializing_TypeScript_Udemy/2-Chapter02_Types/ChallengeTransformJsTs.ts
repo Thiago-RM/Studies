7bb26682d968622a35552854b327b37abad38bf6
Thiago-RM <thiago.rmdf@gmai.com>
2022-04-17 11:01:07 -0300

    [UPDATE]: FILES OF THE CHAPTER TWO CLASS
    ChallengeTransformJsTs.ts, class 32
    Class 33 = Answer to exercise
    update index.html

---
+++ Languages/TypeScript/1-Initializing_TypeScript_Udemy/2-Chapter02_Types/ChallengeTransformJsTs.ts
@@ -0,0 +1,45 @@
+let aula32: string = 'Challenge Lesson 32';
+console.log(`\n\n ${aula32}`);
+
+// In portuguese:
+/*
+*  Transformar os códigos JS abaixo em TS
+* */
+
+/* Exemplo Conta Bancária*/
+type contaBancaria = {
+    saldo: number,
+    depositar: (valor: number) => void
+}
+let contaBancaria: contaBancaria = {
+    saldo: 3456,
+    depositar(valor: number) {
+        this.saldo += valor;
+    }
+
+}
+console.log(contaBancaria.saldo);
+console.log(`Fim da aplicação.`);
+
+/*
+Resetting the application to receive the 3000 deposit from the account
+holder, otherwise, the amount will be added to the previous deposit
+*/
+contaBancaria.saldo = 0;
+
+/* Exemplo Correntista*/
+type correntista = {
+    nome: string,
+    contaBancaria: contaBancaria,
+    contatos: string[];
+}
+
+var correntista: correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['+55(62)612-1212 , +55(62)612-1010']
+}
+
+// Making a deposit
+correntista.contaBancaria.depositar(3000);
+console.log(correntista);
