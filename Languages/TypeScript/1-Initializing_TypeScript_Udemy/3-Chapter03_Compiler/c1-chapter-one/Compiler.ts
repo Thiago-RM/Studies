36e2a53a4f35b89a9b74bb359b181617d548d77c
Thiago-RM <thiago.rmdf@gmai.com>
2022-05-04 21:53:49 -0300

    [UPDATE]: CHAPTER THREE - noEmitOnErro
    update of class 36 - Compiler and noEmitOnError
    Update index.html

---
+++ Languages/TypeScript/1-Initializing_TypeScript_Udemy/3-Chapter03_Compiler/c1-chapter-one/Compiler.ts
@@ -0,0 +1,14 @@
+let class36: string = 'Compiler and "noEmitOnError"';
+console.log(`\n\n ${class36}`);
+
+
+let canal: string = 'Gaveta';
+let inscrito: number = 610234;
+
+/* In portuguese:
+Com essa configuração do compilador impediremos a execução
+ caso haja algum erro no código!
+*/
+
+// canal = inscrito;
+console.log(`Canal = ${canal}`);
