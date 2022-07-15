b606bb01fc0d6011a34a69d0851ccdef70eb52da
Thiago-RM <thiago.rmdf@gmai.com>
2022-07-14 22:45:59 -0300

    [FEAT]: Creat class update
    Class of create update product

--- Front-Frameworks/Angular/udemy1-CursoAngular9/crud/frontend/src/app/app-routing.module.ts
+++ Front-Frameworks/Angular/udemy1-CursoAngular9/crud/frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -20,7 +21,12 @@ const routes: Routes = [
     path: "products/create",
     component: ProductCreateComponent
 
-  }
+  },
+  {
+    path: "products/update/:id",
+    component: ProductUpdateComponent
+
+  },
 ];
 
 @NgModule({
