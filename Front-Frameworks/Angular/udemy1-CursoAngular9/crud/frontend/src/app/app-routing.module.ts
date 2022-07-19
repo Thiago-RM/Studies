d3eaa32deb32515977759c4be29f29e85152ff07
Thiago-RM <thiago.rmdf@gmai.com>
2022-07-19 19:44:37 -0300

    [FEAT]: Creat class delete
    Class of update component delete product

--- Front-Frameworks/Angular/udemy1-CursoAngular9/crud/frontend/src/app/app-routing.module.ts
+++ Front-Frameworks/Angular/udemy1-CursoAngular9/crud/frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { NgModule } from '@angular/core';
@@ -25,6 +26,11 @@ const routes: Routes = [
   {
     path: "products/update/:id",
     component: ProductUpdateComponent
+
+  },
+  {
+    path: "products/delete/:id",
+    component: ProductDeleteComponent
 
   },
 ];
